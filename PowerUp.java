205d2bb03e643cf3a6857164c274c3cd91d4c327
Christofferos <42782387+Christofferos@users.noreply.github.com>
2020-05-31 15:27:46 +0200

    Explanations for powerups added

--- PowerUp.java
+++ PowerUp.java
@@ -9,19 +9,19 @@ public class PowerUp extends Sprite {
         super(xpos, ypos);
         this.type = type;
         if (type == 1) {
-            loadSprite("Images/strengthBoost.png");
+            loadSprite("Images/strengthBoost.png"); // Increase maximum health cap.
         } else if (type == 2) {
-            loadSprite("Images/fireBoost3.png");
+            loadSprite("Images/fireBoost3.png"); // Decreases reload time.
         } else if (type == 3) {
-            loadSprite("Images/movementBoost2.png");
+            loadSprite("Images/movementBoost2.png"); // Increases inventory cap and size of obstacles.
         } else if (type == 4) {
-            loadSprite("Images/minigun.png");
+            loadSprite("Images/minigun.png"); // 7 seconds auto minigun - decreases reload time by 3x. <<< Not yet implemented
         } else if (type == 5) {
-            loadSprite("Images/sniper.png");
+            loadSprite("Images/sniper.png"); // 3 Ammo - Increases bullet speed by 3x. <<< Not yet implemented
         } else if (type == 6) {
-            loadSprite("Images/rocket.png");
+            loadSprite("Images/rocket.png"); // Idle state while manuvering rocket - explodes at collision (even with bullets). <<< Not yet implemented
         } else if (type == 7) {
-            loadSprite("Images/mine.png");
+            loadSprite("Images/mine.png"); // Drops a mine that becomes invisible - explodes at collision with player. <<< Not yet implemented
         }
     }
 
